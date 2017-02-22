79b9e5c8df2999b77a8eed3a70f05ecc443cccc7
Thejudevang <thejudevang@gmail.com>
2017-02-22 21:09:12 +0530

    Initial Commit

---
+++ app/controllers/api/v1/movies_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::MoviesController < ApiController
+
+  def index
+  	movies = Imdb::Top250.new.movies.uniq.map do |object|
+  	  movie = Imdb::Movie.new(object.id)
+
+  	  {
+  	  	id: movie.id,
+  	  	title: movie.title,
+  	  	cast_members: movie.cast_members,
+  	  	poster: movie.poster,
+  	  	rating: movie.rating,
+  	  	trailer_url: movie.trailer_url
+  	  }
+  	end
+
+  	render json: movies
+  end
+
+end
---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+# Load the Rails application.
+require_relative 'application'
+
+# Initialize the Rails application.
+Rails.application.initialize!
+ TWITTER_CONSUMER_KEY= 'aJOlxDeuEspw4BYEG9gnWYS0f'
+TWITTER_CONSUMER_SECRET= 'vd62LEfI6SLQoDy5QLQoCWL6VWp350iEt0yOolMqbtKlfpGD8q' 
